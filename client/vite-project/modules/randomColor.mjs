99096d222a19d81fc9d941ef4e1417006b598b86
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-03-12 15:40:07 +0100

    new color

--- client/vite-project/modules/randomColor.mjs
+++ client/vite-project/modules/randomColor.mjs
@@ -1,11 +1,11 @@
 //let assignedColors = [];
 
 /* export default  */function getRandomColor(assignedColors) {
-  let availableColors = ['#08ff4a', '#ff08de', '#ff8308', '#ff0077', '#ededed', '#b300ff', '#84ab35', '#b07f6d', '#c406d1', '#adadad'];
+  let availableColors = ['#08ff4a', '#ff08de', '#ff8308', '#ff0077', '#00e5ff', '#b300ff', '#84ab35', '#b07f6d', '#c406d1', '#adadad'];
   
  /*  if (availableColors.length === 0) {
     assignedColors = []; // Återställ listan över tilldelade färger när alla färger har använts
-    availableColors = ['#08ff4a', '#ff08de', '#ff8308', '#ff0077', '#ededed', '#b300ff', '#84ab35', '#b07f6d', '#c406d1', '#adadad'];
+    availableColors = ['#08ff4a', '#ff08de', '#ff8308', '#ff0077', '#00e5ff', '#b300ff', '#84ab35', '#b07f6d', '#c406d1', '#adadad'];
   } */
 
   let randomColorIndex = Math.floor(Math.random() * availableColors.length);
