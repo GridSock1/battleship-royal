6e02f2e55328e44ee24c1c99450318dfe37d2596
CarolineVarsaga <carolinevarsaga@gmail.com>
2024-03-13 11:26:26 +0100

    Fixing again

--- client/vite-project/main.js
+++ client/vite-project/main.js
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client';
-//const socket = io('https://goldfish-app-e6acm.ondigitalocean.app');
-const socket = io('http://localhost:3032');
+const socket = io('https://goldfish-app-e6acm.ondigitalocean.app');
+//const socket = io('http://localhost:3032');
 //import getRandomColor from './modules/randomColor.mjs';
 import './game/game.js';
  
